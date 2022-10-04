dc01da473fe46fc406cd16c5653b49fbfa64f382
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:23:12 +0000

    Adding numeric_owner as keyword arguement

--- checkrel.py
+++ checkrel.py
@@ -153,7 +153,7 @@ def find_dependencies(file_data: Dict):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar_file, extract_dir)
